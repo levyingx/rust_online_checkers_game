ea1ac85d2fe5e496dd3881d4fd72f5682f1ab1bc
Levy <112917687+levyingx@users.noreply.github.com>
2024-06-18 01:59:54 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
+// emojis: ⚪🔴⬛⬜
+
 use std::io;
+
+fn init_board(board: &mut [[char; 8]; 8]) -> &[[char; 8]; 8] {
+  for i in 0..8 {
+    for j in 0..8 {
+      if i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0 {
+        board[i][j] = '⬜';
+      } else {
+        board[i][j] = '⬛';
+      }
+    }
+  }
+
+  for i in 0..3 { // board red disc init
+    for j in 0..8 {
+      if i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0 {
+        board[i][j] = '🔴';
+      } 
+    }
+  }
+
+  for i in 5..8 { // board white discs init
+    for j in 0..8 {
+      if i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0 {
+        board[i][j] = '⚪';
+      } 
+    }
+  }
+
+  return board;
+}
 
 fn display_board(board: [[char; 8]; 8]) {
   for i in 0..8 {
@@ -9,39 +41,28 @@ fn display_board(board: [[char; 8]; 8]) {
   }
 }
 
-fn move_piece(mut board: [[char; 8]; 8]) {
-
+fn move_piece() {
+  
 }
 
 fn main() {
-  let mut board = [['⬛'; 8]; 8];
+  let mut board = [['⬛'; 8]; 8]; 
+  let mut start_x = String::new();
+  let mut start_y = String::new();
+  let mut end_x = String::new();
+  let mut end_y = String::new();
+  
+  init_board(&mut board); // init board
+  display_board(board); // display board 
 
-  for i in 0..8 {
-    for j in 0..8 {
-      if i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0 {
-        board[i][j] = '⬜';
-      }
-    }
-  }
+  println!("Start (x):");
+  io::stdin().read_line(&mut start_x).unwrap();
+  start_x = start_x.trim().parse().unwrap();
+  println!("Start (y):");
+  io::stdin().read_line(&mut start_y).unwrap();
+  println!("End (x):");
+  io::stdin().read_line(&mut end_x).unwrap();
+  println!("End (y):");
+  io::stdin().read_line(&mut end_y).unwrap();
 
-  for i in 0..3 {
-    for j in 0..8 {
-      if i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0 {
-        board[i][j] = '🔴';
-      }
-    }
-  }
-
-  for i in 5..8 {
-    for j in 0..8 {
-      if i % 2 == 0 && j % 2 != 0 || i % 2 != 0 && j % 2 == 0 {
-        board[i][j] = '⚪';
-      }
-    }
-  }
-
-  // display the board
-  // prompt the user to enter the move
-  // move the piece
-  // send the updated board data to the other screen
 }
